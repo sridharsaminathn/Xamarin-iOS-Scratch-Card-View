c187181c339857168c639c1e9b36686f7a5c1fcb
Sridhar Saminathan <ssaminathan@abbvie.com>
2020-07-04 23:18:08 +0530

    Scratch Card Added
    Scratch Card Added

---
+++ XamariniOSScratchCardView/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace XamariniOSScratchCardView
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            scratchArea.Image = UIImage.FromBundle("scratch_img");
+            scratchArea.BackgroundColor = UIColor.White.ColorWithAlpha(0.5f);
+            scratchArea.ScratchCompleted += ScratchArea_ScratchCompleted;
+        }
+
+        private void ScratchArea_ScratchCompleted(int obj)
+        {
+            lblScratchPercentage.Text = string.Format("Scratch {0}% Completed", obj);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ XamariniOSScratchCardView/ViewController.designer.cs
@@ -0,0 +1,47 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace XamariniOSScratchCardView
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView after_scratching { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblScratchPercentage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        XamariniOSScratchCardView.ScratchCardView scratchArea { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (after_scratching != null) {
+                after_scratching.Dispose ();
+                after_scratching = null;
+            }
+
+            if (lblScratchPercentage != null) {
+                lblScratchPercentage.Dispose ();
+                lblScratchPercentage = null;
+            }
+
+            if (scratchArea != null) {
+                scratchArea.Dispose ();
+                scratchArea = null;
+            }
+        }
+    }
+}
